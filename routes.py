f2c6d35fc70f849dc60b17f9e201ec88da025dae
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-04 19:06:51 -0300

    feat: Configuração de rotas e navegação com menu lateral
    - Adicionada rota para a nova página "Quiz" no menu de navegação.
    - Implementada lógica de navegação com base no índice selecionado no `NavigationDrawer`.
    - Adicionadas e configuradas rotas para as páginas: Home, Questions, About, Score e Quiz.
    - Lógica de navegação aprimorada, com controle de histórico e gerenciamento de views.

--- routes.py
+++ routes.py
@@ -1,16 +1,19 @@
 import flet as ft
 from loguru import logger
 from pages.questions_page import QuestionPage
+from pages.quiz_page import QuizPage
 from pages.home_page import HomePage
 from pages.about_page import AboutPage
 from pages.score_page import ScorePage
+
 
 def setup_routes(page: ft.Page):
     logger.info("Configurando rotas")
 
     def change_route(e):
         selected_index = e.control.selected_index
-        logger.info(f"Alterando rota com base no índice selecionado: {selected_index}")
+        logger.info(f"Alterando rota com base no índice selecionado: {
+                    selected_index}")
         if selected_index == 0:
             page.go('/home')
         elif selected_index == 1:
@@ -18,13 +21,16 @@ def setup_routes(page: ft.Page):
         elif selected_index == 2:
             page.go('/about')
         elif selected_index == 3:
-            page.go('/score')  # Adicionando rota para a página de pontuação
+            page.go('/score')
+        elif selected_index == 4:
+            page.go('/quiz')
 
     # NavigationDrawer
     drawer = ft.NavigationDrawer(
         controls=[
             ft.Container(
-                content=ft.Text("Menu Principal", size=24, weight=ft.FontWeight.BOLD),
+                content=ft.Text("Menu Principal", size=24,
+                                weight=ft.FontWeight.BOLD),
                 padding=ft.padding.all(10),
             ),
             ft.Divider(height=1),
@@ -43,10 +49,15 @@ def setup_routes(page: ft.Page):
                 icon=ft.icons.INFO_OUTLINED,
                 selected_icon_content=ft.Icon(ft.icons.INFO),
             ),
-            ft.NavigationDrawerDestination(  # Adicionando a opção de Score no menu
+            ft.NavigationDrawerDestination(
                 label="Score",
                 icon=ft.icons.SCOREBOARD_OUTLINED,
                 selected_icon_content=ft.Icon(ft.icons.SCOREBOARD),
+            ),
+            ft.NavigationDrawerDestination(
+                label="Quiz",
+                icon=ft.icons.LIST_ALT_OUTLINED,
+                selected_icon_content=ft.Icon(ft.icons.LIST_ALT),
             ),
         ],
         on_change=change_route
@@ -57,58 +68,64 @@ def setup_routes(page: ft.Page):
     def route_change(route):
         logger.info(f"Rota alterada para: {route}")
         if page.route == "/home":
-            page.title = "Home - Painel Alison dev"
-            if not page.views or page.views[-1].route != '/home':
-                page.views.clear()
-                page.views.append(
-                    ft.View(
-                        route='/home',
-                        appbar=ft.AppBar(title=ft.Text("Home")),
-                        controls=[HomePage(page)],
-                        drawer=drawer,
-                    )
+            page.title = "Home - Painel"
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    route='/home',
+                    appbar=ft.AppBar(title=ft.Text("Home")),
+                    controls=[HomePage(page)],
+                    drawer=drawer,
                 )
+            )
         elif page.route == "/questions":
-            page.title = "Questions - Painel Alison dev"
-            if not page.views or page.views[-1].route != '/questions':
-                page.views.clear()
-                page.views.append(
-                    ft.View(
-                        route='/questions',
-                        appbar=ft.AppBar(title=ft.Text("Questions")),
-                        scroll=ft.ScrollMode.HIDDEN,
-                        controls=[QuestionPage(page)],
-                        drawer=drawer,
-                    )
+            page.title = "Questions - Painel"
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    route='/questions',
+                    appbar=ft.AppBar(title=ft.Text("Questions")),
+                    controls=[QuestionPage(page)],
+                    drawer=drawer,
                 )
+            )
+        elif page.route == "/quiz":
+            page.title = "Quiz - Painel"
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    route='/quiz',
+                    appbar=ft.AppBar(title=ft.Text("Quiz")),
+                    controls=[QuizPage(page)],
+                    scroll=ft.ScrollMode.AUTO,
+                    drawer=drawer,
+                )
+            )
         elif page.route == "/about":
-            page.title = "About - Painel Alison dev"
-            if not page.views or page.views[-1].route != '/about':
-                page.views.clear()
-                page.views.append(
-                    ft.View(
-                        route='/about',
-                        scroll=ft.ScrollMode.AUTO,
-                        appbar=ft.AppBar(title=ft.Text("About")),
-                        controls=[AboutPage(page)],
-                        drawer=drawer,
-                    )
+            page.title = "About - Painel"
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    route='/about',
+                    appbar=ft.AppBar(title=ft.Text("About")),
+                    controls=[AboutPage(page)],
+                    drawer=drawer,
                 )
+            )
         elif page.route == "/score":
-            page.title = "Score - Painel Alison dev"  # Título para a página de pontuação
-            if not page.views or page.views[-1].route != '/score':
-                page.views.clear()
-                page.views.append(
-                    ft.View(
-                        route='/score',
-                        scroll=ft.ScrollMode.AUTO,
-                        appbar=ft.AppBar(title=ft.Text("Score")),
-                        controls=[ScorePage(page)],  # Chamando a página de pontuação
-                        drawer=drawer,
-                    )
+            page.title = "Score - Painel"
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    route='/score',
+                    appbar=ft.AppBar(title=ft.Text("Score")),
+                    controls=[ScorePage(page)],
+                    drawer=drawer,
                 )
+            )
         else:
-            logger.warning(f"Rota desconhecida: {route}, redirecionando para /home")
+            logger.warning(f"Rota desconhecida: {
+                           route}, redirecionando para /home")
             page.go('/home')
             return
         page.update()
