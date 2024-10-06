7ba9a3dd084b1adde7360344381be03cdd40b70f
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-06 20:08:14 -0300

    feat: Adicionada funcionalidade de dicas no renderizador de perguntas
    - Implementada a exibição de dicas para cada pergunta utilizando o componente `IconButton` com um ícone de lâmpada.
    - A função `mostrar_dica` foi adicionada para exibir a dica na tela por meio de uma barra de notificação.
    - Melhorada a interação do usuário ao fornecer dicas diretamente na interface durante o quiz.

--- components/question_render.py
+++ components/question_render.py
@@ -3,7 +3,7 @@ from components.answer import Answer
 from utils.score_calculator import calcular_perguntas
 
 
-def renderizar_perguntas(perguntas, question_container, answer_instances, page):
+def renderizar_perguntas(perguntas, question_container, answer_instances, page, hints):
     question_container.controls.clear()
     answer_instances.clear()
 
@@ -12,6 +12,9 @@ def renderizar_perguntas(perguntas, question_container, answer_instances, page):
         opcoes_resposta = pergunta_obj['opcoes']
         letras_opcoes = pergunta_obj['letras_opcoes']
         resposta_correta_letra = pergunta_obj['resposta_letra']
+
+        hint = hints[idx -
+                     1] if hints and len(hints) >= idx else "Nenhuma dica disponível."
 
         question_container.controls.append(
             ft.Text(f"{idx}. {pergunta_texto}",
@@ -28,3 +31,27 @@ def renderizar_perguntas(perguntas, question_container, answer_instances, page):
 
         question_container.controls.append(answer_group)
         answer_instances.append(answer_group)
+
+        question_container.controls.append(
+            ft.Row(
+                controls=[
+                    ft.IconButton(
+                        icon=ft.icons.LIGHTBULB_OUTLINE,
+                        tooltip="Ver Dica",
+                        on_click=lambda e, hint_text=hint: mostrar_dica(
+                            page, hint_text)
+                    )
+                ]
+            )
+        )
+
+
+def mostrar_dica(page, hint_text):
+    # Exibir "Nenhuma dica disponível" se a dica estiver faltando
+    hint_display = hint_text if hint_text else "Nenhuma dica disponível."
+    page.snack_bar = ft.SnackBar(
+        content=ft.Text(f"Dica: {hint_display}"),
+        bgcolor=ft.colors.AMBER_400
+    )
+    page.snack_bar.open = True
+    page.update()
