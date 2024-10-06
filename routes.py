816cc2a43a50950b8f1be01bc8af6464704c888d
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-06 20:01:57 -0300

    feat: Suporte a página 404 e ajuste de rolagem na página de resultados
    - Adicionada rota para página 404 para lidar com rotas desconhecidas.
    - Adicionada rolagem à página de resultados para permitir navegação em grandes resultados.
    - Implementada função `create_drawer` para criar o menu de navegação em todas as páginas.

--- routes.py
+++ routes.py
@@ -5,65 +5,12 @@ from pages.quiz_page import QuizPage
 from pages.home_page import HomePage
 from pages.about_page import AboutPage
 from pages.score_page import ScorePage
+from pages.page_404 import PageNotFound
+from components.drawer import create_drawer
 
 
 def setup_routes(page: ft.Page):
     logger.info("Configurando rotas")
-
-    def change_route(e):
-        selected_index = e.control.selected_index
-        logger.info(f"Alterando rota com base no índice selecionado: {
-                    selected_index}")
-        if selected_index == 0:
-            page.go('/home')
-        elif selected_index == 1:
-            page.go('/questions')
-        elif selected_index == 2:
-            page.go('/about')
-        elif selected_index == 3:
-            page.go('/score')
-        elif selected_index == 4:
-            page.go('/quiz')
-
-    # NavigationDrawer
-    drawer = ft.NavigationDrawer(
-        controls=[
-            ft.Container(
-                content=ft.Text("Menu Principal", size=24,
-                                weight=ft.FontWeight.BOLD),
-                padding=ft.padding.all(10),
-            ),
-            ft.Divider(height=1),
-            ft.NavigationDrawerDestination(
-                label="Home",
-                icon=ft.icons.HOME_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.HOME),
-            ),
-            ft.NavigationDrawerDestination(
-                label="Questions",
-                icon=ft.icons.QUESTION_ANSWER_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.QUESTION_ANSWER),
-            ),
-            ft.NavigationDrawerDestination(
-                label="About",
-                icon=ft.icons.INFO_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.INFO),
-            ),
-            ft.NavigationDrawerDestination(
-                label="Score",
-                icon=ft.icons.SCOREBOARD_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.SCOREBOARD),
-            ),
-            ft.NavigationDrawerDestination(
-                label="Quiz",
-                icon=ft.icons.LIST_ALT_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.LIST_ALT),
-            ),
-        ],
-        on_change=change_route
-    )
-
-    page.drawer = drawer
 
     def route_change(route):
         logger.info(f"Rota alterada para: {route}")
@@ -72,10 +19,10 @@ def setup_routes(page: ft.Page):
             page.views.clear()
             page.views.append(
                 ft.View(
+                    vertical_alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                     route='/home',
-                    appbar=ft.AppBar(title=ft.Text("Home")),
                     controls=[HomePage(page)],
-                    drawer=drawer,
                 )
             )
         elif page.route == "/questions":
@@ -86,7 +33,7 @@ def setup_routes(page: ft.Page):
                     route='/questions',
                     appbar=ft.AppBar(title=ft.Text("Questions")),
                     controls=[QuestionPage(page)],
-                    drawer=drawer,
+                    drawer=create_drawer(page),
                 )
             )
         elif page.route == "/quiz":
@@ -98,7 +45,7 @@ def setup_routes(page: ft.Page):
                     appbar=ft.AppBar(title=ft.Text("Quiz")),
                     controls=[QuizPage(page)],
                     scroll=ft.ScrollMode.AUTO,
-                    drawer=drawer,
+                    drawer=create_drawer(page),
                 )
             )
         elif page.route == "/about":
@@ -109,7 +56,7 @@ def setup_routes(page: ft.Page):
                     route='/about',
                     appbar=ft.AppBar(title=ft.Text("About")),
                     controls=[AboutPage(page)],
-                    drawer=drawer,
+                    drawer=create_drawer(page),
                 )
             )
         elif page.route == "/score":
@@ -120,14 +67,22 @@ def setup_routes(page: ft.Page):
                     route='/score',
                     appbar=ft.AppBar(title=ft.Text("Score")),
                     controls=[ScorePage(page)],
-                    drawer=drawer,
+                    scroll=ft.ScrollMode.AUTO,  # Adicionando rolagem para grandes resultados
+                    drawer=create_drawer(page),
                 )
             )
         else:
             logger.warning(f"Rota desconhecida: {
-                           route}, redirecionando para /home")
-            page.go('/home')
-            return
+                           route}, redirecionando para 404")
+            page.views.clear()
+            page.views.append(
+                ft.View(
+                    route='/404',
+                    appbar=ft.AppBar(title=ft.Text("Página Não Encontrada")),
+                    controls=[PageNotFound(page)],
+                    drawer=create_drawer(page),
+                )
+            )
         page.update()
 
     def view_pop(view):
