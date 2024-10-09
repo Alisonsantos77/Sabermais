2be6cb0850a413f922c9248700bc31c8b08ac49a
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-09 18:49:33 -0300

    refactor(thumbnail_generator): melhorar geração de thumbnail com verificação de existência e uso do Pillow
    - Substituído o uso do ffmpeg diretamente pela biblioteca Pillow para gerar thumbnails.
    - Adicionada verificação para reutilizar thumbnails existentes, evitando geração desnecessária.
    - Melhorada a manipulação de erros com detalhamento das falhas na geração da thumbnail.

--- services/thumbnail_generator.py
+++ services/thumbnail_generator.py
@@ -1,9 +1,14 @@
-import ffmpeg
+import os
+import subprocess
+from PIL import Image
 
 
-def generate_thumbnail(video_path, thumbnail_path, time="00:00:02"):
+def generate_thumbnail_with_pillow(video_path, thumbnail_path, time="00:00:02"):
     """
-    Gera uma thumbnail do vídeo no tempo especificado.
+    Gera uma thumbnail do vídeo no tempo especificado usando Pillow.
+    
+    Se o arquivo de thumbnail já existir, ele é reutilizado. Caso contrário, uma nova
+    thumbnail é gerada a partir do vídeo.
 
     :param video_path: Caminho do arquivo de vídeo.
     :param thumbnail_path: Caminho onde a thumbnail será salva.
@@ -11,13 +16,25 @@ def generate_thumbnail(video_path, thumbnail_path, time="00:00:02"):
     :return: Caminho para a thumbnail gerada ou None em caso de erro.
     """
     try:
-        (
-            ffmpeg
-            .input(video_path, ss=time)  # Captura a thumbnail no tempo especificado
-            .output(thumbnail_path, vframes=1, update=1, pix_fmt="yuvj420p")
-            .run(overwrite_output=True, quiet=True)
-        )
+        if os.path.exists(thumbnail_path):
+            print(
+                f"Arquivo {thumbnail_path} já existe. Utilizando o existente.")
+            return thumbnail_path
+
+        print(f"Gerando thumbnail para: {video_path}")
+        result = subprocess.run([
+            'ffmpeg',
+            '-ss', time,
+            '-i', video_path,
+            '-frames:v', '1',
+            '-q:v', '2',
+            '-vf', 'scale=320:-1',
+            thumbnail_path
+        ], check=True, capture_output=True, text=True)
+
+        print(f"Thumbnail gerada em: {thumbnail_path}")
         return thumbnail_path
-    except ffmpeg.Error as e:
-        print(f"Erro ao gerar thumbnail: {str(e)}")
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao gerar thumbnail com Pillow: {str(e)}")
+        print(f"Detalhes do erro: {e.stderr}")
         return None
