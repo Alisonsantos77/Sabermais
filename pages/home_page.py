19af7899760c77be33ad062e6556dc90a39ee0e0
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-09 18:46:38 -0300

    refactor: Renomear aplicativo para 'Saber+' e atualizar interface de boas-vindas
    - Alterado o texto de boas-vindas para "Bem-vindo ao Saber+!".
    - Adicionada imagem do mascote ao layout da página inicial.
    - Alterado o texto de introdução para "Vamos começar?".
    - Substituído botão elevado por um ícone de seta para navegar para a análise.

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,38 +4,39 @@ import flet as ft
 def HomePage(page: ft.Page):
 
     def navigate_to_questions(e):
-        # Animação de loading pode ser adicionada aqui se necessário
         page.go("/questions")
 
     return ft.Container(
         padding=ft.padding.all(20),
         content=ft.Column(
-            controls=[
-                ft.Text(
-                    value="Bem-vindo ao VideoQ!",
-                    size=28,
-                    color=ft.colors.ON_SURFACE,
-                    weight=ft.FontWeight.BOLD
-                ),
-                ft.Text(
-                    value="Acesse suas perguntas geradas automaticamente para estudo.",
-                    size=16,
-                    color=ft.colors.ON_SURFACE_VARIANT,
-                ),
-                ft.ElevatedButton(
-                    text="Ir para perguntas",
-                    icon=ft.icons.QUESTION_ANSWER,
-                    on_click=navigate_to_questions,
-                    bgcolor=ft.colors.PRIMARY,
-                    color=ft.colors.ON_PRIMARY,  
-                    style=ft.ButtonStyle(
-                        shape=ft.RoundedRectangleBorder(radius=10),
-                        elevation=3, 
-                    ),
-                ),
-            ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=20,  # Espaço entre elementos
+            controls=[
+                ft.Text(
+                    value="Bem-vindo ao Saber+!",
+                    size=28,
+                    color=ft.colors.PRIMARY,
+                    weight=ft.FontWeight.BOLD
+                ),
+                ft.Image(
+                    src="/images/mascote/acenando_frente.png",
+                    fit=ft.ImageFit.CONTAIN,
+                    height=300,
+                    width=300,
+                ),
+                ft.Text(
+                    value="Vamos começar?",
+                    weight=ft.FontWeight.BOLD,
+                    size=24,
+                    color=ft.colors.PRIMARY,
+                ),
+                ft.IconButton(
+                    icon=ft.icons.ARROW_FORWARD,
+                    icon_color=ft.colors.PRIMARY,
+                    tooltip="Ir para análise",
+                    on_click=lambda _: page.go("/questions"),
+                ),
+            ],
+            spacing=20,
         )
     )
