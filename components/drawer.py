2655e56a9b8f1ffc63762902178e36e94a03276f
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-09 18:43:57 -0300

    feat: adicionar suporte a tema e atualizar layout do drawer
    - Alterado o título do drawer para "Saber+" e ajustada a cor do texto para o tema primário.
    - Substituída a imagem do logo por "icon_windows.png".
    - Removida a opção "Home" e reorganizados os destinos do drawer.
    - Adicionada opção de tema com base no modo de tema armazenado no client_storage.

--- components/drawer.py
+++ components/drawer.py
@@ -2,28 +2,25 @@ import flet as ft
 
 
 def create_drawer(page: ft.Page):
+    theme = page.client_storage.get("theme_mode")
     return ft.NavigationDrawer(
         controls=[
             ft.Container(
-                content=ft.ResponsiveRow(
+                content=ft.Row(
                     controls=[
+                        ft.Text("Saber+", size=30, weight=ft.FontWeight.BOLD,
+                                color=ft.colors.PRIMARY),
                         ft.Image(
-                            src="/images/logosaber.png",
+                            src="icon_windows.png",
                             width=100,
                             height=100,
                             fit=ft.ImageFit.CONTAIN,
                         ),
-                        ft.Text("VideoQ", size=30, weight=ft.FontWeight.BOLD),
                     ],
                 ),
                 padding=ft.padding.all(10),
             ),
             ft.Divider(height=1),
-            ft.NavigationDrawerDestination(
-                label="Home",
-                icon=ft.icons.HOME_OUTLINED,
-                selected_icon_content=ft.Icon(ft.icons.HOME),
-            ),
             ft.NavigationDrawerDestination(
                 label="Questions",
                 icon=ft.icons.QUESTION_ANSWER_OUTLINED,
@@ -44,6 +41,12 @@ def create_drawer(page: ft.Page):
                 icon=ft.icons.SCOREBOARD_OUTLINED,
                 selected_icon_content=ft.Icon(ft.icons.SCOREBOARD),
             ),
+            ft.Divider(height=1),
+            ft.NavigationDrawerDestination(
+                label=f"Theme {theme}",
+                icon=ft.icons.WB_SUNNY_OUTLINED,
+                selected_icon_content=ft.Icon(ft.icons.WB_SUNNY),
+            ),
         ],
         on_change=lambda e: handle_drawer_change(e, page)
     )
@@ -52,12 +55,10 @@ def create_drawer(page: ft.Page):
 def handle_drawer_change(e, page):
     selected_index = e.control.selected_index
     if selected_index == 0:
-        page.go('/home')
-    elif selected_index == 1:
         page.go('/questions')
-    elif selected_index == 2:
+    elif selected_index == 1:
         page.go('/quiz')
-    elif selected_index == 3:
+    elif selected_index == 2:
         page.go('/about')
-    elif selected_index == 4:
+    elif selected_index == 3:
         page.go('/score')
