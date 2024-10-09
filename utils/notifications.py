234c8805013e70fa25651a1f510e110a5a3202a8
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-09 18:52:06 -0300

    refactor: Substituir a função de notificação por uma implementação com ToastNotifier
    - Removida a função `show_notification_plyer` que utilizava print para exibir notificações.
    - Implementada a função `show_notification` que utiliza a biblioteca `win10toast` para exibir notificações no Windows com ícone e duração.
    - Adicionado suporte para tocar um áudio durante a notificação.
    - Verificações para garantir que o ícone e as mensagens sejam válidos.

--- utils/notifications.py
+++ utils/notifications.py
@@ -1,9 +1,41 @@
-def show_notification_plyer(title, message, max_length=100):
-    if message is None:
-        message = "Nenhuma mensagem disponível."
+from win10toast import ToastNotifier
+import time
+import os
+import flet as ft
 
-    # Limitar a mensagem ao tamanho máximo, se necessário
-    display_message = (message[:max_length] +
-                       '...') if len(message) > max_length else message
+# Inicializar o ToastNotifier
+toaster = ToastNotifier()
 
-    print(f"{title}: {display_message}")
+icon_path = "assets/images/icon_wintoast.ico"
+sound_path = "assets/audios/notification_song.wav"
+
+
+def show_notification(page, title, message, icon_path, duration=5):
+    if not os.path.exists(icon_path):
+        print(f"Ícone não encontrado: {icon_path}")
+        icon_path = None
+
+    if not title or not message:
+        print("Título ou mensagem inválida.")
+        return
+
+    try:
+        audio = ft.Audio(src=sound_path, autoplay=True, volume=0.5)
+        page.controls.append(audio)
+        page.update()
+    except Exception as e:
+        print(f"Erro ao tocar o som: {e}")
+
+    toaster.show_toast(
+        title,
+        message,
+        icon_path=icon_path,
+        duration=duration,
+        threaded=True,
+    )
+
+    time.sleep(0.5)
+
+    # Verificar se a notificação ainda está ativa
+    while toaster.notification_active():
+        time.sleep(0.1)
