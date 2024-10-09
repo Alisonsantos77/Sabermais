5bb05d33fa0937da3757bf8a7f901889c5966bd6
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-09 18:45:09 -0300

    refactor: atualizar função de manipulação de arquivos para usar Pillow
    - Substituída a função de geração de miniaturas para usar 'generate_thumbnail_with_pillow'.
    - Alterado o parâmetro 'wallpaper_container' para 'mascote'.
    - Atualizada a lógica de exibição da miniatura para usar o objeto 'mascote'.
    - Atualizada a imagem padrão para uma versão triste do mascote se nenhum arquivo for selecionado.

--- components/file_picker_handler.py
+++ components/file_picker_handler.py
@@ -1,8 +1,8 @@
 import flet as ft
-from services.thumbnail_generator import generate_thumbnail
+from services.thumbnail_generator import generate_thumbnail_with_pillow
 
 
-def on_file_picker_result(e: ft.FilePickerResultEvent, page, wallpaper_container, selected_file):
+def on_file_picker_result(e: ft.FilePickerResultEvent, page, mascote, selected_file):
     if e.files:
         file_name = e.files[0].name
         video_path = e.files[0].path
@@ -11,16 +11,18 @@ def on_file_picker_result(e: ft.FilePickerResultEvent, page, wallpaper_container
 
         file_size = round(e.files[0].size / (1024 * 1024), 2)
 
-        thumbnail_path = generate_thumbnail(
-            video_path, f"thumbnail_{file_name}.jpg")
+        thumbnail_filename = f"thumbnail_{file_name}.jpg"
 
-        wallpaper_container.current.image_src = thumbnail_path
-        wallpaper_container.current.update()
+        thumbnail_path = generate_thumbnail_with_pillow(
+            video_path, thumbnail_filename)
 
-        selected_file.value = f"Arquivo: {
-            file_name}\nTamanho: {file_size} MB\n"
+        if thumbnail_path:
+            mascote.current.src = thumbnail_path
+            mascote.current.update()
+
+        selected_file.value = f"Arquivo: {file_name}\nTamanho: {file_size} MB\n"
     else:
         selected_file.value = "Nenhum arquivo selecionado"
-        wallpaper_container.current.image_src = "https://images8.alphacoders.com/136/1363709.png"
-        wallpaper_container.current.update()
+        mascote.current.src = "/images/mascote/triste.png" 
+        mascote.current.update()
     page.update()
